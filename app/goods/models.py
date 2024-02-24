373ce3823adb66ddf2f8657dafb48cbe6cf3fe39
Daniil <157521979+GoToProd@users.noreply.github.com>
2024-02-24 18:22:26 +0300

    Admin panel setup

--- app/goods/models.py
+++ app/goods/models.py
@@ -1,7 +1,5 @@
-from email.policy import default
-from enum import unique
-from tabnanny import verbose
 from django.db import models
+from django.urls import reverse
 
 
 class Categories(models.Model):
@@ -37,8 +35,14 @@ class Products(models.Model):
     def __str__(self):
         return f'{self.name} - Колличество - {self.quantity}'
     
+    
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
+    
     def display_id(self):
         return f"{self.id:05}"
+    
     
     def sell_price(self):
         if self.discount:
